2fb088283ebc321b35dfb4256192f0fad4863c47
David Manjarres <davidmanjarres2004@gmail.com>
2025-08-20 20:36:57 -0500

    Add basic user module microservice

---
+++ userms/src/users/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+  
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  name: string;
+}
